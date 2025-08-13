054c89bcecfa5ba6353a1150fed8320dd4a2a533
alvar.dev <alvaropedrozo07@gmail.com>
2025-08-13 00:14:05 -0500

    feat: Add nullable fields x, y, and page to ContractTemplateFieldsEntity

--- src/contract/contract_templates_fields.entity.ts
+++ src/contract/contract_templates_fields.entity.ts
@@ -30,6 +30,15 @@ export class ContractTemplateFieldsEntity {
   @Column()
   type: string;
 
+  @Column({ nullable: true })
+  x: number
+
+  @Column({ nullable: true })
+  y: number
+  
+  @Column({ nullable: true })
+  page: number
+
   @ManyToOne(() => ContractTemplateEntity, entity => entity.fields)
   @JoinColumn({ name: 'ct_id', referencedColumnName: 'ct_id' })
   contractTemplate: ContractTemplateEntity;
