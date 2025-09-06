5b88e8ed7859fe89acae868cc9a67d624540ffb3
Alvaro Pedrozo <alvaropedrozo07@gmail.com>
2025-09-06 16:38:59 -0500

    refactor(files): remove unused audit log imports from files module

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -2,8 +2,6 @@ import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { UserEntity } from "src/user/user.entity"
-import { AuditLogService } from "src/audit_logs/audit.service"
-import { AuditLogsEntity } from "src/audit_logs/audit.entity"
 import { FilesController } from "./files.controller"
 import { FilesService } from "./files.service"
 
