4192c6e9bbb62af44142f4a29ee0377891aa8060
Alvaro Pedrozo <alvaropedrozo07@gmail.com>
2025-12-02 12:19:51 -0500

    Disable PM2 cluster mode and max instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,6 @@ module.exports = {
     {
       name: "contractme-api",
       script: "./dist/main.js",
-      instances: "max",
-      exec_mode: "cluster",
       env_production: {
         NODE_ENV: "prod",
         DD_LOGS_INJECTION: "true",
