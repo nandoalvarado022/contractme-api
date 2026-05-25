ede3212c1a8b303a1b689e559328e6040e3046a1
alvar.dev <alvaropedrozo07@gmail.com>
2026-05-24 21:34:14 -0500

    refactor: remove balance reconciliation service and controller from cron module

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -1,13 +1,11 @@
-import { Module } from "@nestjs/common";
-import { ScheduleModule } from "@nestjs/schedule";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ConfigModule } from "@nestjs/config";
-import { BalanceReconciliationService } from "./balance-reconciliation.service";
-import { DatabaseBackupService } from "./database-backup.service";
-import { ReconciliationController } from "./reconciliation.controller";
-import { BackupController } from "./backup.controller";
-import { BalanceEntity } from "../entities/balance/entities/balance.entity";
-import { TransactionsEntity } from "../entities/transactions/entities/transactions.entity";
+import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseBackupService } from './database-backup.service';
+import { BackupController } from './backup.controller';
+import { BalanceEntity } from '../entities/balance/entities/balance.entity';
+import { TransactionsEntity } from '../entities/transactions/entities/transactions.entity';
 
 @Module({
   imports: [
@@ -15,8 +13,8 @@ import { TransactionsEntity } from "../entities/transactions/entities/transactio
     ConfigModule,
     TypeOrmModule.forFeature([BalanceEntity, TransactionsEntity]),
   ],
-  controllers: [ReconciliationController, BackupController],
-  providers: [BalanceReconciliationService, DatabaseBackupService],
-  exports: [BalanceReconciliationService, DatabaseBackupService],
+  controllers: [BackupController],
+  providers: [DatabaseBackupService],
+  exports: [DatabaseBackupService],
 })
 export class CronModule {}
